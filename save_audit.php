07279153a1bd3bc759ea6e7cb85b5027ab224c44
avornusolomon6-maker <avornusolomon6@gmail.com>
2025-10-18 15:33:50 +0000

    Update save_audit.php

--- save_audit.php
+++ save_audit.php
@@ -19,4 +19,10 @@ try {
 } catch (Exception $e) {
     echo json_encode(["status" => "error", "message" => $e->getMessage()]);
 }
+
+    function save_audit_log($conn, $username, $userType, $action, $description) {
+    $stmt = $conn->prepare("INSERT INTO logs (username, user_type, actions, description) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$username, $userType, $action, $description]);
+}
+    
 ?>
