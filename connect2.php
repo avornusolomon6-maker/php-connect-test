379a424681ab7b7bbcf9d00272071a95121907e9
avornusolomon6-maker <avornusolomon6@gmail.com>
2026-07-25 23:27:11 +0000

    Create connect2.php

---
+++ connect2.php
@@ -0,0 +1,20 @@
+<?php
+$host = "ep-muddy-fire-a8ypvpo1-pooler.eastus2.azure.neon.tech";
+$dbname = "umate";
+$user = "neondb_owner";
+$pass = "npg_L39rfXYTGupW";
+$sslmode = "require";
+
+try {
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname;sslmode=$sslmode", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     //echo "Connected successfully to Neon!";
+} catch (PDOException $e) {
+    //echo json_encode([
+       // "status" => "error",
+       // "message" => "Database connection failed: " . $e->getMessage()
+   // ]);
+    exit;
+}
+?>
+
