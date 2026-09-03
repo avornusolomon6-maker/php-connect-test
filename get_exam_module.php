a4f33b0ef021ae327351bb9ca1bf0ba960f3ef0e
avornusolomon6-maker <avornusolomon6@gmail.com>
2026-09-03 12:33:38 +0000

    Create get_exam_module.php

---
+++ get_exam_module.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: application/json');
+require_once 'connect.php';
+
+try {
+    $sql = "SELECT * FROM settings";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$row) {
+        echo json_encode([
+            "success" => false,
+            "message" => "Data not found, please contact your administrator"
+        ]);
+        exit;
+    }
+
+    echo json_encode([
+        "success" => true,
+        "module" => $row['module'],
+        "edit_program"  => $row['program_change']
+    ]);
+
+} catch (Exception $e) {
+    echo json_encode([
+        "success" => false,
+        "message" => $e->getMessage()
+    ]);
+}
